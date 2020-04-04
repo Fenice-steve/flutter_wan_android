40bb10ae4db169b7098c875b10120833a375a607
sunpanyue <18816284864@163.com>
2020-04-04 22:57:05 +0800

    ChangeProvider

--- lib/config/provider_manager.dart
+++ lib/config/provider_manager.dart
@@ -6,9 +6,12 @@ List<SingleChildWidget> providers = [
 
 ];
 
-/// 需要依赖的model
-///
+/**
+ * ChangeNotifierProxyProvider组合了两种功能：
+    （1）它会自动订阅CartModel中的更改（如果您只想使用此功能，只需使用ChangeNotifierProvider）
+    （2）它采用先前提供的对象的值（在本例中为CatalogModel，如上所述），并使用它来构建CartModel的值（如果您只需要此功能，只需使用ProxyProvider）
+ */
 ///  UserModel依赖globalFavouriteStateModel
-//List<SingleChildWidget> dependentServices = [
-//  ChangeNotifierProxyProvider<UserModel>(create: null, update: null)
-//];
+List<SingleChildWidget> dependentServices = [
+  ChangeNotifierProxyProvider<UserModel>(create: null, update: null)
+];
